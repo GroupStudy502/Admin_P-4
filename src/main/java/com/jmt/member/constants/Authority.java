dcef8e0131ac67a97f26e9c2cf53342d68ef2896
hjchoirr <hjchoir@naver.com>
2024-08-13 17:13:32 +0900

    api config 저장 디버그

--- src/main/java/com/jmt/member/constants/Authority.java
+++ src/main/java/com/jmt/member/constants/Authority.java
@@ -1,7 +1,17 @@
 package com.jmt.member.constants;
 
 public enum Authority {
-    ALL,
-    USER,
-    ADMIN
+    ALL("전체"),
+    USER("일반"),
+    ADMIN("관리자");
+
+    private final String title;
+
+    Authority(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
